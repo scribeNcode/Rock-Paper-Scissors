f774e34df06358723c4c1f315d30a91334a4f4ff
scribencode <deyeye99@gmail.com>
2025-12-12 00:44:08 +0100

    Write the logic to get the computer choice
    computer chooses either 'rock', 'papper' or
    'scissors'

---
+++ script.js
@@ -0,0 +1,15 @@
+// logic to get the computer choice
+function getComputerChoice(){
+   let randomNum = Math.floor(Math.random()*3)
+   if(randomNum === 0){
+    return 'rock'
+   }else if(randomNum === 1){
+    return 'paper'
+   }else if(randomNum === 2){
+    return 'scissors'
+   }else{
+    return 'error'
+   }
+}
+
+console.log(getComputerChoice())
